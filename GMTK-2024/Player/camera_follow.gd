f942f971b04c86571c77c46b94ebbb97a5c50697
Douglas Hale <115197311+DrDoughnuts@users.noreply.github.com>
2024-08-17 21:08:26 -0400

    Camera y offset

--- GMTK-2024/Player/camera_follow.gd
+++ GMTK-2024/Player/camera_follow.gd
@@ -1,6 +1,7 @@
 extends Node2D
 @export var view_distance: float = 200.0
 @export_range(0, 1, 0.05) var mouse_distance_threshold: float = 0.75
+@export var yoffset: float = -100.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -9,3 +10,4 @@ func _process(_delta: float) -> void:
 	if dist_offset < mouse_distance_threshold:
 		dist_offset = 0
 	position = diff_vector.normalized() * view_distance * dist_offset
+	position.y += yoffset
