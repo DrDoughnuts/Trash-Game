d26349c516fc1420560cb5853dc2fdc349e30ad8
Douglas Hale <115197311+DrDoughnuts@users.noreply.github.com>
2024-08-20 01:02:39 -0400

    replay button works

--- GMTK-2024/Winning/again.gd
+++ GMTK-2024/Winning/again.gd
@@ -1,12 +1,13 @@
 extends Button
-@onready var level := preload("res://GMTK-2024/THE TRASH COMPACTOR.tscn")
-
+var level
 
 func _ready() -> void:
 	$"../Sfx_victory".play()
+	
 
 
 func _on_pressed() -> void:
 	Trashmanager.trash = 0
 	Trashmanager.time_left = Trashmanager.start_time
-	get_tree().change_scene_to_packed(level)
+	level = load("res://GMTK-2024/THE TRASH COMPACTOR.tscn")
+	get_tree().change_scene_to_packed.call_deferred(level)
--- GMTK-2024/Winning/arrow.gd
+++ GMTK-2024/Winning/arrow.gd
@@ -4,4 +4,4 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
-		get_tree().change_scene_to_packed(win_scene)
+		get_tree().change_scene_to_packed.call_deferred(win_scene)
