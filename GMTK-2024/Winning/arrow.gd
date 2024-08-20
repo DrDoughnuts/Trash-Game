992036531d514aa610977ac187b512e8bd706362
Douglas Hale <115197311+DrDoughnuts@users.noreply.github.com>
2024-08-19 22:26:09 -0400

    Start of Win screen

---
+++ GMTK-2024/Winning/arrow.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+@onready var win_scene = preload("res://GMTK-2024/Winning/winning.tscn")
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		get_tree().change_scene_to_packed(win_scene)
